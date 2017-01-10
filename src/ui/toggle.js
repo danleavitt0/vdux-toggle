0c7827fd1c238128843c30d7733c8667a0888795
danleavitt0 <danleavitt0@gmail.com>
2017-01-09 18:19:40 -0800

    add option for right label

--- src/ui/toggle.js
+++ src/ui/toggle.js
@@ -13,8 +13,22 @@ import flatten from 'flatten'
 function render ({props, children}) {
   const {
     w, name, label, labelClass, color = 'secondary', bgColor = 'primary',
-    labelProps = {}, active, ...restProps
+    labelPosition = 'left', labelProps = {}, active, ...restProps
   } = props
+
+  const labelBlock = (
+    <Block
+      tag='label'
+      align='start center'
+      w='calc(100% - 38px)'
+      relative
+      overflow='visible'
+      cursor='pointer'
+      {...labelProps}
+      class={'vui-toggle-label'}>
+      {label}
+    </Block>
+  )
 
   return (
     <Block
@@ -27,19 +41,7 @@ function render ({props, children}) {
       {...restProps}>
       <Input absolute wide tall opacity='0' pointerEvents='none' name={name} value={active} type='checkbox'/>
       <Flex cursor='pointer' align='start center'>
-        {label && (
-          <Block
-            tag='label'
-            align='start center'
-            w='calc(100% - 38px)'
-            relative
-            overflow='visible'
-            cursor='pointer'
-            {...labelProps}
-            class={'vui-toggle-label'}>
-            {label}
-          </Block>
-        )}
+        {(label && labelPosition === 'left') && labelBlock}
         <Block relative p='4px 0px 4px 2px' w='36px'>
           <Block
             wide
@@ -62,6 +64,7 @@ function render ({props, children}) {
             pointerEvents='none'
             boxShadow='rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px' />
         </Block>
+        {(label && labelPosition === 'right') && labelBlock}
       </Flex>
     </Block>
 
